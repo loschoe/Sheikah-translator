da66f65ef72fb4e360c93bad3510971b11189fae
SCHOEPF Louis <loschoe@cse-strasbourg.com>
2021-12-19 15:52:23 +0100

    Voici la suite du projet en amélio

---
+++ interface_de_graven.py
@@ -0,0 +1,107 @@
+
+# Importation du module Tkinter
+from tkinter import *
+# importation du module webbrowser afin d'afficher les sites webs 
+import webbrowser 
+
+# Déf de la variable pour ouvrir les tutos
+def oppen_graven_tuto():
+	webbrowser.open_new("https://www.youtube.com/watch?v=N4M4W7JPOL4")
+
+def oppen_second_tuto():
+	webbrowser.open_new("https://info.blaisepascal.fr/tkinter")
+
+def oppen_tres_tuto():
+	webbrowser.open_new("https://python.doctor/page-tkinter-interface-graphique-python-tutoriel") 
+
+# créer une fenetre 
+window = Tk()
+
+#personaliser la fenetre 
+window.title ("Interface de Graven")
+window.geometry("1080x720")
+window.minsize(480, 360)
+window.config (background= 'darkgreen')
+
+# ajouter un premier text à la fenetre
+label_title = Label (window, text="Bienvenue sur le translator", font=("Arial", 30), bg= 'darkgreen', fg= 'white')  
+label_title.pack()
+
+# ajouter un second text à la fenetre
+label_subtitle = Label (window, text="Découvre comment il fonctionne et traduit tes messages en Sheikah", font=("Arial", 15), bg= 'darkgreen', fg= 'white')  
+label_subtitle.pack()
+
+# ajouter un permier bouton qui ouvre des pages webs 
+yt_bouton= Button(text="Aides au codage", font=("Arial", 30), bg= 'white', fg= 'darkgreen', command=lambda:[oppen_graven_tuto(), oppen_second_tuto(), oppen_tres_tuto()]) 
+yt_bouton.pack()
+
+# ajouter un bouton pour fermer la page 
+
+qb = Button(window, text="Fermer", font=("Arial", 30), bg= 'white', fg= 'darkgreen', command=window.quit)
+qb.place (x=100, y=0) 
+
+# Affichage du bouton dans «window»
+qb.pack()
+
+
+# afficher 
+window.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- interface_sheika.py
+++ interface_sheika.py
@@ -26,28 +26,8 @@ entr2.grid(row=1, column=1)
 
 window.geometry("1080x720")
 qb = Button(window, text='Bravo Louis !', command=window.quit)
+
+
 window.mainloop()
 
-def alert():
-    showinfo("alerte", "Bravo!")
 
-menubar = Menu(fenetre)
-
-menu1 = Menu(menubar, tearoff=0)
-menu1.add_command(label="Créer", command=alert)
-menu1.add_command(label="Editer", command=alert)
-menu1.add_separator()
-menu1.add_command(label="Quitter", command=fenetre.quit)
-menubar.add_cascade(label="Fichier", menu=menu1)
-
-menu2 = Menu(menubar, tearoff=0)
-menu2.add_command(label="Couper", command=alert)
-menu2.add_command(label="Copier", command=alert)
-menu2.add_command(label="Coller", command=alert)
-menubar.add_cascade(label="Editer", menu=menu2)
-
-menu3 = Menu(menubar, tearoff=0)
-menu3.add_command(label="A propos", command=alert)
-menubar.add_cascade(label="Aide", menu=menu3)
-
-fenetre.config(menu=menubar)
