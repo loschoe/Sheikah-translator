16b5a355dee4eb47c4058b004011625fc71e9721
SCHOEPF Louis <loschoe@cse-strasbourg.com>
2022-01-12 10:44:19 +0100

    Envoie de la bonne copie

--- Mon_Translator.py
+++ Mon_Translator.py
@@ -1,6 +1,6 @@
 
 # Importation du module Tkinter
-from tkinter import *
+from tkinter import*
 # importation du module webbrowser afin d'afficher les sites webs 
 import webbrowser 
 
@@ -18,6 +18,8 @@ def lettre_a(event):
 	print ("test")
 # Faire les autres events et pas fini 
 
+
+
 # créer la fenetre 
 window = Tk()
 
@@ -30,7 +32,10 @@ window.config (background= 'darkgreen')
 
 # creation de la frame
 # masquer pour utiliser le cadrillage 
-#frame = Frame(window, bg= 'darkgreen')
+#frame = Frame(window, bg= 'darkblue', bd=0)
+#tree = ttk.Treeview(frame, columns = (1,2,3), height = 5, show = "headings")
+#tree.pack(side = 'left')
+
 
 
 # ajouter un premier text à la fenetre "titre"
@@ -39,30 +44,35 @@ label_title = Label (window, text="Bienvenue sur le translator", font=("Arial", 
 label_title.grid(row=0, column=3 , sticky=N)
 
 # ajouter un second text à la fenetre "texte"
+# Alligné !!
 label_subtitle = Label (window, text="Découvre comment il fonctionne et traduit tes messages en Sheikah", font=("Arial", 15), bg= 'darkgreen', fg= 'white')  
 label_subtitle.grid(row=2, column=3, sticky=N)
 
 # ajouter un permier bouton "Aides au codage"
-# Parfait emplacement !
+# Alligné !!
 yt_bouton= Button(text="Aides au codage", font=("Arial", 15), bg= 'white', fg= 'darkgreen', command=lambda:[oppen_graven_tuto(), oppen_second_tuto(), oppen_tres_tuto()]) 
-yt_bouton.grid(row=0, column = 2, sticky=W)
+yt_bouton.grid(row=0, column=2, sticky=W)
 
 # ajouter un bouton pour "fermer la page" 
-# Bonton parfait !!
+# Alligné !!
 quit_bouton = Button(text="Fermer", font=("Arial", 15), bg= 'white', fg= 'darkgreen', command=window.quit)  
-quit_bouton.grid(row=0, column = 1, sticky=W)
+quit_bouton.grid(row=0, column=1, sticky=W)
 
 # ajouter de la zone de text 
+# Alligné !!
+text_fr = Text(window, width=40, height=4, font=("Arial", 20))
+text_fr.grid(row=10, column=3, sticky=N)
 
 # Création du bouton "Traduire "
+### JE VOUDRAIS L'ESPACER !!!!!!
 trad_bouton = Button(window, text="Traduire", font=("Arial", 25), bg= 'white', fg= 'darkgreen')
-trad_bouton.grid(row=15, column=2, sticky=N)
+trad_bouton.grid(row=12, column= 3, sticky=N)
 
-# zone_de_text_fr.bind('<a>', lettre_a) --> à réfléchire 
 
 
 # afficher la frame
-#frame.grid(row=0, column=0, sticky=W)
+#frame.grid(row=13, column=3, sticky=N)
 
 # afficher la fenetre
 window.mainloop()
+
